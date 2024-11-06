6e0dacf0130da75dcecd311b140b381a167328b3
gr1shan1a <368409@edu.itmo.ru>
2024-11-06 04:36:16 +0300

    new_old files

---
+++ src/main/java/backend/academy/Maze_game/ConsoleRenderer.java
@@ -0,0 +1,50 @@
+package backend.academy.Maze_game;
+
+import java.util.List;
+
+public class ConsoleRenderer implements Renderer { 
+
+    @Override
+    public String render(Maze maze) {
+        StringBuilder sb = new StringBuilder();
+        for (int row = 0; row < maze.height(); row++) {
+            for (int col = 0; col < maze.width(); col++) {
+                Coordinate current = new Coordinate(row, col);
+                if (current.equals(maze.start())) {
+                    sb.append('A'); // Start point
+                } else if (current.equals(maze.end())) {
+                    sb.append('B'); // End point
+                } else if (maze.getCell(row, col).type() == Cell.Type.WALL) {
+                    sb.append('#');
+                } else {
+                    sb.append('.'); // Free space
+                }
+            }
+            sb.append('\n'); // New line
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public String render(Maze maze, List<Coordinate> path) {
+        StringBuilder sb = new StringBuilder();
+        for (int row = 0; row < maze.height(); row++) {
+            for (int col = 0; col < maze.width(); col++) {
+                Coordinate current = new Coordinate(row, col);
+                if (current.equals(maze.start())) {
+                    sb.append('A'); // Start point
+                } else if (current.equals(maze.end())) {
+                    sb.append('B'); // End point
+                } else if (path.contains(current)) {
+                    sb.append('*'); // Path point
+                } else if (maze.getCell(row, col).type() == Cell.Type.WALL) {
+                    sb.append('#'); // Wall
+                } else {
+                    sb.append('.'); // Free space
+                }
+            }
+            sb.append('\n'); // New line
+        }
+        return sb.toString();
+    }
+}
